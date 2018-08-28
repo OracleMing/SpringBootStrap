6e8dfcb8e34de1789960702fb72a3d3e52e9463b
wxmvbn <547573024@qq.com>
2018-08-28 13:35:36 +0800

    升级至Spring Boot2.0，更新配置。

--- src/main/java/com/wxm/JavaWeb.java
+++ src/main/java/com/wxm/JavaWeb.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 10 * 60)
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 10 * 60)
 public class JavaWeb {
 
 	public static void main(String[] args) {
